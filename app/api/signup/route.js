6c665ea005abcb800554ff28c44cd8eb5a5be2ee
salauddin046 <kssalauddin@intellicar.in>
2026-05-15 13:12:53 +0530

    update

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,35 +1,38 @@
-import { Pool } from 'pg'
+export async function GET() {
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-})
+  return Response.json({
+    success: true,
+    message:
+      'Signup API Working',
+  })
+}
 
-export async function POST(req) {
+export async function POST(
+  request
+) {
+
   try {
-    const body = await req.json()
+
+    const body =
+      await request.json()
 
     const {
       name,
       email,
-      username,
       password,
     } = body
 
-    const existing = await pool.query(
-      `
-      SELECT *
-      FROM users
-      WHERE username = $1
-      OR email = $2
-      `,
-      [username, email]
-    )
+    if (
+      !name ||
+      !email ||
+      !password
+    ) {
 
-    if (existing.rows.length > 0) {
       return Response.json(
         {
-          error:
-            'Username or email already exists',
+          success: false,
+          message:
+            'All fields required',
         },
         {
           status: 400,
@@ -37,36 +40,26 @@ export async function POST(req) {
       )
     }
 
-    await pool.query(
-      `
-      INSERT INTO users (
-        name,
-        email,
-        username,
-        password
-      )
-      VALUES ($1, $2, $3, $4)
-      `,
-      [
-        name,
-        email,
-        username,
-        password,
-      ]
-    )
-
     return Response.json({
+
       success: true,
+
       message:
         'Account created successfully',
+
+      user: {
+        name,
+        email,
+      },
     })
 
   } catch (error) {
-    console.error(error)
 
     return Response.json(
       {
-        error: 'Signup failed',
+        success: false,
+        message:
+          'Server error',
       },
       {
         status: 500,
