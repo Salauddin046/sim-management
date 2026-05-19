51a5e47ce682ddc774dc04361beddf63065d70ef
salauddin046 <kssalauddin@intellicar.in>
2026-05-19 12:22:07 +0530

    update

--- app/api/command-center/route.js
+++ app/api/command-center/route.js
@@ -33,20 +33,50 @@ export async function GET() {
 
         `
         SELECT DISTINCT
-
         SUBSTRING(
           activation_date,
           4,
           9
         ) AS month
-
         FROM sim_data3
+        WHERE activation_date IS NOT NULL
+        AND activation_date <> ''
 
-        WHERE
-        activation_date IS NOT NULL
+        UNION
 
-        AND
-        activation_date <> ''
+        SELECT DISTINCT
+        SUBSTRING(
+          termination_date,
+          4,
+          9
+        ) AS month
+        FROM sim_data3
+        WHERE termination_date IS NOT NULL
+        AND termination_date <> ''
+
+        UNION
+
+        SELECT DISTINCT
+        SUBSTRING(
+          safe_custody_move_in,
+          4,
+          9
+        ) AS month
+        FROM sim_data3
+        WHERE safe_custody_move_in IS NOT NULL
+        AND safe_custody_move_in <> ''
+
+        UNION
+
+        SELECT DISTINCT
+        SUBSTRING(
+          safe_custody_move_out,
+          4,
+          9
+        ) AS month
+        FROM sim_data3
+        WHERE safe_custody_move_out IS NOT NULL
+        AND safe_custody_move_out <> ''
 
         ORDER BY month
         `
