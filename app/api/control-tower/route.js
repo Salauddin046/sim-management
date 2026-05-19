98b7d5ff3f5e19b27b80b288875c79a974039a66
salauddin046 <kssalauddin@intellicar.in>
2026-05-19 15:05:40 +0530

    update

--- app/api/control-tower/route.js
+++ app/api/control-tower/route.js
@@ -1,67 +1,10 @@
 export async function GET() {
 
-  try {
+  return Response.json({
 
-    const response =
-      await fetch(
+    success: true,
 
-        'https://airtelsim.intellicar.in/api/v1/airtel/sims/list',
-
-        {
-
-          method: 'POST',
-
-          headers: {
-
-            accept:
-              'application/json, text/plain, */*',
-
-            authorization:
-              'Basic YWlydGVsYXBpOkFpcnRlSW50ZWxsaWNhckAjMTIzNDU=',
-
-            'content-type':
-              'application/json',
-
-            origin:
-              'https://airtelsim.intellicar.in',
-
-            referer:
-              'https://airtelsim.intellicar.in/analysis',
-
-            cookie:
-              `PASTE_FULL_COOKIE_HERE`,
-          },
-
-          body: JSON.stringify({
-
-            page_no: 1,
-          }),
-        }
-      )
-
-    const text =
-      await response.text()
-
-    console.log(text)
-
-    return new Response(
-      text,
-      {
-        headers: {
-          'Content-Type':
-            'application/json',
-        },
-      }
-    )
-
-  } catch (error) {
-
-    return Response.json({
-
-      success: false,
-
-      message:
-        error.message,
+    message:
+      'API working',
     })
-  }
 }
