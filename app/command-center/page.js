9a0e194bc04be1b6b9a3bacd3e98e81e5fd44979
salauddin046 <kssalauddin@intellicar.in>
2026-05-19 12:07:25 +0530

    Update

--- app/command-center/page.js
+++ app/command-center/page.js
@@ -220,7 +220,7 @@ export default function CommandCenterPage() {
               <p className="
                 text-gray-500
               ">
-                
+                PostgreSQL SIM Database Search
               </p>
 
             </div>
@@ -269,8 +269,7 @@ Search SIM / Phone / Client / Device ID
               "
             />
 
-            <input
-              type="month"
+            <select
               value={month}
               onChange={(e) =>
                 setMonth(
@@ -283,7 +282,61 @@ Search SIM / Phone / Client / Device ID
                 p-4
                 outline-none
               "
-            />
+            >
+
+              <option value="">
+                Select Month
+              </option>
+
+              <option value="2025-01">
+                Jan 2025
+              </option>
+
+              <option value="2025-02">
+                Feb 2025
+              </option>
+
+              <option value="2025-03">
+                Mar 2025
+              </option>
+
+              <option value="2025-04">
+                Apr 2025
+              </option>
+
+              <option value="2025-05">
+                May 2025
+              </option>
+
+              <option value="2025-06">
+                Jun 2025
+              </option>
+
+              <option value="2025-07">
+                Jul 2025
+              </option>
+
+              <option value="2025-08">
+                Aug 2025
+              </option>
+
+              <option value="2025-09">
+                Sep 2025
+              </option>
+
+              <option value="2025-10">
+                Oct 2025
+              </option>
+
+              <option value="2025-11">
+                Nov 2025
+              </option>
+
+              <option value="2025-12">
+                Dec 2025
+              </option>
+
+            </select>
 
             <button
               onClick={
